b5ffb1ae14eda51e6b322843b0f0e5e56d71d723
JimeRosello <jimena.rosello@gmail.com>
2018-06-28 16:19:55 -0300

    Add files via upload

--- grails-app/domain/pomodoro/Board.groovy
+++ grails-app/domain/pomodoro/Board.groovy
@@ -3,11 +3,18 @@ package pomodoro
 class Board {
 
     LinkedList<Task> toDo = new LinkedList<Task>();
-    LinkedList<Task> inProgress = new LinkedList<Task>();
+    Task inProgress;
     LinkedList<Task> done = new LinkedList<Task>();
 
-    public Task getNextTask() {
-      return toDo.sort(it.priority).first();
+    public void addPendingTask(Task task) {
+      toDo.add(task);
+      toDo = toDo.sort{x,y -> x.priority <=> y.priority ?: x.estimatedPomodoros <=> y.estimatedPomodoros};
+    }
+
+    public void completeTaskInProgress() {
+      done.add(inProgress);
+      inProgress = toDo.first();
+      toDo.remove(inProgress);
     }
 
     static constraints = {
