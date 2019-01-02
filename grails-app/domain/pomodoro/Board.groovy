34e117a7553cf87685595211d5bdb604e13fbab2
Jimena Rosello <jimena.rosello@gmail.com>
2019-01-01 23:21:23 -0300

    Correction in Board class

--- grails-app/domain/pomodoro/Board.groovy
+++ grails-app/domain/pomodoro/Board.groovy
@@ -2,22 +2,21 @@ package pomodoro
 
 class Board {
 
-    LinkedList<Task> toDo = new LinkedList<Task>() 
-    Task inProgress 
-    LinkedList<Task> done = new LinkedList<Task>() 
-    // estado de task
+    LinkedList<Task> toDo = new LinkedList<Task>()
+    Task inProgress
+    LinkedList<Task> done = new LinkedList<Task>()
 
     public void addPendingTask(Task task) {
-      toDo.add(task) 
+      toDo.addLast(task)
       toDo = toDo.sort {
         x, y -> x.priority <=> y.priority ?: x.estimatedPomodoros <=> y.estimatedPomodoros
-      } 
+      }
     }
 
     public void completeTaskInProgress() {
-      done.add(inProgress) 
-      inProgress = toDo.first() 
-      toDo.remove(inProgress) 
+      done.addLast(inProgress)
+      inProgress = toDo.first()
+      toDo.remove(inProgress)
     }
 
     static constraints = {
