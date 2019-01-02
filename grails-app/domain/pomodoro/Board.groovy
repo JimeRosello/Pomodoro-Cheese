82ef82b30b258715db03e7c41bd73ea2a64eee41
Jimena Rosello <jimena.rosello@gmail.com>
2019-01-01 23:32:23 -0300

    Correction in sorting method

--- grails-app/domain/pomodoro/Board.groovy
+++ grails-app/domain/pomodoro/Board.groovy
@@ -8,15 +8,19 @@ class Board {
 
     public void addPendingTask(Task task) {
       toDo.addLast(task)
-      toDo = toDo.sort {
-        x, y -> x.priority <=> y.priority ?: x.estimatedPomodoros <=> y.estimatedPomodoros
-      }
+      sortByPriorityAndEstimatedPomodoros()
     }
 
     public void completeTaskInProgress() {
       done.addLast(inProgress)
       inProgress = toDo.first()
       toDo.remove(inProgress)
+    }
+
+    private void sortByPriorityAndEstimatedPomodoros() {
+      toDo = toDo.sort {
+        x, y -> x.priority <=> y.priority ?: x.estimatedPomodoros <=> y.estimatedPomodoros
+      }
     }
 
     static constraints = {
